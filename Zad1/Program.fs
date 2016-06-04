0cd1dea2fd89be70f1ebc21f49c69d2789e0c446
Pavlo <Pavlo@DESKTOP-4N3MQ06>
2016-06-04 22:55:48 +0200

    zad1, zad2

--- Zad1/Program.fs
+++ Zad1/Program.fs
@@ -1,7 +1,25 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿
+// Zrealizuj w języku F# funkcję typu char * string -> int zliczającą liczbę
+// wystąpień podanego znaku w łańcuchu tekstowym
+
+open System
+
+let countChar (character : char) (str : string) = 
+    let mutable count = 0
+    for ch in str do
+        if ch = character
+            then count <- count + 1
+    count
+
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    let char = 'r'
+    let world = "Programming"
+    let result = countChar char world
+
+    Console.WriteLine("There are " + result.ToString() + " occurrence of " + char.ToString() + " in the " + world + ".")
+
+    Console.ReadLine() |> ignore
+
     0 // return an integer exit code
---
+++ Zad2/Program.fs
@@ -0,0 +1,23 @@
+﻿
+// Napisz funkcję w języku F# sumującą liczby znajdujące się w liście będącej
+// parametrem tej funkcji (typ funkcji: float list -> float).
+
+open System
+
+let sumList list =
+    let mutable result = 0
+    for i in list do
+        result <- result + i
+    result 
+
+[<EntryPoint>]
+let main argv = 
+
+    let list = [1; 2; 3]
+    let result = sumList list
+
+    Console.WriteLine("There result are " + result.ToString())
+
+    Console.ReadLine() |> ignore
+
+    0 // return an integer exit code
