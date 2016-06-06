f3197f43822e2cc118b3c79335f994668f5541de
Pavlo <Pavlo@DESKTOP-4N3MQ06>
2016-06-06 17:10:59 +0200

    ZadZal_E_1

---
+++ ZadZal_E_1/Program.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System
+
+let sum list = 
+    list 
+    |> List.filter (fun e -> e  > 0)
+    |> List.sum
+
+[<EntryPoint>]
+let main argv =
+    let list = [1; -2; 4]
+
+    let result = sum list
+ 
+    printfn "%i" result
+
+    Console.ReadLine() |> ignore
+    0 // return an integer exit code
