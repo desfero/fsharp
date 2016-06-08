b08cab7bfe2479ec1fb6849578be8646e20d10d0
Pavlo <Pavlo@DESKTOP-4N3MQ06>
2016-06-08 22:28:51 +0200

    zad z zaliczenia Asi

---
+++ Zad_Zal_Asi/Program.fs
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System
+
+let rec create n x =
+    match n with
+    | 0 -> []
+    | _ -> create (n - 1) x @ [x + n - 1]      
+
+[<EntryPoint>]
+let main argv = 
+    
+    printfn "%A" (create 3 5)
+
+    Console.ReadLine() |> ignore
+
+    0 // return an integer exit code
