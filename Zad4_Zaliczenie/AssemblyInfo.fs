c57103a249a1576b21a3807be6d0f900b88f6a4c
Pavlo <Pavlo@DESKTOP-4N3MQ06>
2016-06-08 15:40:27 +0200

    nowe zadanie

---
+++ Zad4_Zaliczenie/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace Zad4_Zaliczenie.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Zad4_Zaliczenie")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Zad4_Zaliczenie")>]
+[<assembly: AssemblyCopyright("Copyright ©  2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("5d32df31-08e1-4955-8930-87504fee1a74")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ Zad4_Zaliczenie/Program.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System
+let potega x n =
+    let mutable result = x
+    if n >= 0 then 
+       result <- 1
+       for i = 1 to n do
+         result <- result * x
+    result
+        
+
+[<EntryPoint>]
+let main argv = 
+    System.Console.WriteLine(potega 2 3)
+
+    Console.ReadLine() |> ignore
+
+    0 // return an integer exit code
